cdf2059013860d49b0d319400f5da6e3ef619a6d
李华志 <lhz5975y@163.com>
2018-10-24 21:36:31 +0800

    Initial commit

---
+++ src/com/company/Demo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Demo {
+    public static void main(String[] args){
+        int i = 0;
+        int j = 0;
+        System.out.println(i/(j+1));
+        System.out.println("hello world");
+        System.out.println("Demo.main");
+    }
+}
